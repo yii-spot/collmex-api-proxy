d5b229393d1b4f8d45c3f02a837f54e48bc04c68
Thomas Udalrik <t.udalrik@yii-spot.com>
2022-02-26 17:21:04 +0100

    Composer require overblog/graphql-bundle and overblog/graphiql-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,7 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Overblog\GraphQLBundle\OverblogGraphQLBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Overblog\GraphiQLBundle\OverblogGraphiQLBundle::class => ['dev' => true],
 ];
